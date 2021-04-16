df2533f0e359836450e45f80997ade4e8f6b4aec
ezeHumar <humarezequiel@gmail.com>
2021-04-15 23:59:40 -0300

    Project created and amplify backend added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyUIAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,6 +54,12 @@
  *
  */
 
+
+
+(window as any).global = window;
+(window as any).process = {
+    env: { DEBUG: undefined },
+};
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
